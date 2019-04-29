b7d6de915749fc84b58faa4b76fbc32ab6d375a0
clarkexcore <clarkexcore@gmail.com>
2019-04-29 10:55:39 -0400

    First Commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+class App extends Component {
+    render() {
+        return (
+            <View>
+                <Text>Here is SOME text.</Text>
+            </View>
+        );
+    }
+}
+
+export default App;
